89a04c93ab6cfbf9c9dc29c0435bf4ef172f6acd
Aditya Singh <adityasingh740745@gmail.com>
2024-10-31 14:36:07 +0530

    add responsiveness across diff screen sizes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const lenis  = new Lenis();
+const lenis = new Lenis();
 
 function raf(time) {
     lenis.raf(time);
@@ -7,66 +7,69 @@ function raf(time) {
 requestAnimationFrame(raf);
 
 function injectCards() {
-    const container = document.querySelector('.container');
+    const container = document.querySelector(".container");
     const cardsData = [
         {
-            title: 'Algorithm',
-            image: 'assets/images/1.png',
-            description: "The algorithm's workings are shrouded in complexity."
-        }, 
-        {
-            title: 'Dogma',
-            image: 'assets/images/2.png',
-            description: "Enshrining the principles of conformity and reinforcing the status quo."
+            title: "Algorithm",
+            image: "assets/images/1.png",
+            description: "The algorithm's workings are shrouded in complexity.",
         },
         {
-            title: 'Architects',
-            image: 'assets/images/3.png',
-            description: "The elusive entities, lacking human form, operate in the shadows."
+            title: "Dogma",
+            image: "assets/images/2.png",
+            description:
+                "Enshrining the principles of conformity and reinforcing the status quo.",
         },
         {
-            title: 'Wasteland',
-            image: 'assets/images/4.png',
-            description: "This overlooked realm, a consequence of algorithmic judgments."
+            title: "Architects",
+            image: "assets/images/3.png",
+            description:
+                "The elusive entities, lacking human form, operate in the shadows.",
         },
         {
-            title: 'Narrative',
-            image: 'assets/images/5.png',
-            description: "The collective story sculpted by the architects."
+            title: "Wasteland",
+            image: "assets/images/4.png",
+            description:
+                "This overlooked realm, a consequence of algorithmic judgments.",
         },
         {
-            title: 'Opulence',
-            image: 'assets/images/6.png',
-            description: "The cognitive elite's wealth in the algorithmic society."
-        }
-    ]
-    
-    cardsData.map(cardData => {
-        const card = `<div class="card sticky top-28 w-[32vw] bg-[#2F251E] text-white text-center rounded-xl flex flex-col items-center gap-5 py-28">
-                <div class="card-image w-[15vw] aspect-square">
+            title: "Narrative",
+            image: "assets/images/5.png",
+            description: "The collective story sculpted by the architects.",
+        },
+        {
+            title: "Opulence",
+            image: "assets/images/6.png",
+            description: "The cognitive elite's wealth in the algorithmic society.",
+        },
+    ];
+
+    cardsData.map((cardData) => {
+        const card = `<div class="card sticky top-8 md:top-28 w-[90%] sm:w-[80%] md:w-[65%] lg:w-[50%] 2xl:w-[32vw] bg-[#2F251E] text-white text-center rounded-xl flex flex-col items-center gap-5 py-28">
+                <div class="card-image w-[30vw] md:w-[20vw] xl:w-[15vw] aspect-square">
                     <img src="${cardData.image}">
                 </div>
                 <div class="card-content">
-                    <h2 class="text-7xl font-light uppercase">The <br> <span class="font-bold">${cardData.title}</span></h2>
-                    <p class="w-[80%] mx-auto text-2xl leading-[1] font-light mt-10">${cardData.description}</p>
+                    <h2 class="text-5xl lg:text-7xl font-light uppercase">The <br> <span class="font-bold">${cardData.title}</span></h2>
+                    <p class="w-full min-[540px]:w-[90%] lg:w-[80%] mx-auto text-xl lg:text-2xl leading-[1] font-light mt-10">${cardData.description}</p>
                 </div>
-            </div>`
+            </div>`;
 
         container.innerHTML += card;
-    })
+    });
 }
 injectCards();
 
-const cards = document.querySelectorAll('.card').forEach(card => {
+const cards = document.querySelectorAll(".card").forEach((card) => {
     gsap.to(card, {
         opacity: 0,
-        scale: .8,
+        scale: 0.8,
         scrollTrigger: {
             trigger: card,
-            start: 'top 112px',
-            end: 'bottom 112px',
+            start: "top 112px",
+            end: "bottom 112px",
             // markers: true,
-            scrub: 1.5
-        }
-    })
-})
+            scrub: 1.5,
+        },
+    });
+});
